92647fb71e384ceceebf57b867b81545fc9c9b11
chandrakanth patil <chandrakanthpatil0202@gmail.com>
2025-12-18 16:01:06 +0530

    Fix production API URL

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,13 +1,13 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:5000/api"
+  baseURL: "https://event-rsvp-api.onrender.com/api",
 });
 
-api.interceptors.request.use((config) => {
+api.interceptors.request.use((req) => {
   const token = localStorage.getItem("token");
-  if (token) config.headers.Authorization = token;
-  return config;
+  if (token) req.headers.Authorization = `Bearer ${token}`;
+  return req;
 });
 
 export default api;
